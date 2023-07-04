49950787b3edd1735f5633cba05865a1ba37eb57
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2023-07-05 00:12:43 +0200

    WaveTrack::InsertClip & its use instead of push_back to maintain order and tempo information.

--- libraries/lib-wave-track/WaveTrack.h
+++ libraries/lib-wave-track/WaveTrack.h
@@ -585,7 +585,8 @@ private:
    //
 
    /*!
-    @invariant all are non-null and match `this->GetWidth()`
+    * Do not call `mClips.push_back` directly. Use `InsertClip` instead.
+    * @invariant all are non-null and match `this->GetWidth()`
     */
    WaveClipHolders mClips;
 
@@ -612,6 +613,9 @@ private:
 
    //! Whether all clips have a common rate
    bool RateConsistencyCheck() const;
+
+   //! Inserts in order, beside doing other maintenance work
+   void InsertClip(WaveClipHolder clip);
 
    SampleBlockFactoryPtr mpFactory;
 
