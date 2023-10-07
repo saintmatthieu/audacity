7bda3165fa3c1a25ceddf00c09b8392e049b7a51
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2023-10-07 14:39:25 +0200

    Document method

--- libraries/lib-clip-analysis/ClipAnalysis.cpp
+++ libraries/lib-clip-analysis/ClipAnalysis.cpp
@@ -16,6 +16,27 @@ double GetBpmLogLikelihood(double bpm)
    return -0.5 * arg * arg;
 }
 
+// Assuming that odfVals are onsets of a loop. Then there must be a round number
+// of beats. Currently assuming a 4/4 time signature, it just tried a range of
+// number of measures and picks the most likely one. Which is ?..
+//
+// Fact number 1: tempi abide by a distribution that might well be approximated
+// with a gaussian PDF. Looking up some numbers on the net, this could have an
+// average around 115 bpm and a variance of 25 bpm. This is given a weight in
+// the comparison of hypotheses.
+//
+// Second, in most musical genres, and in any case those we expect to see the
+// most in a DAW, a piece is a recursive rhythmic construction: the sections of
+// a piece may have common time divisors ; the number of bars in a section is
+// often a multiple of 2 ; and then a bar, as time signatures suggest, is
+// divisible most often in 4 (4/4), sometimes in 3 (3/4, 6/8) and sometimes
+// in 2.
+//
+// Here we begin simple and assume 4/4. Remains just finding the number of bars.
+// The rest is currently an improvisation using the auto correlation of the
+// onset detection values, which works halfway, but something more sensible and
+// discriminant might be at hand ...
+
 std::optional<double> GetBpm(const std::vector<double>& odfVals, double playDur)
 {
    constexpr auto maxNumBars = 8u;
@@ -25,6 +46,8 @@ std::optional<double> GetBpm(const std::vector<double>& odfVals, double playDur)
       double bpm;
    };
 
+   // The approach below may not be that discriminant, but first results looking
+   // okay were obtained with it.
    std::vector<Score> scores;
    for (auto numBars = 1u; numBars <= maxNumBars; ++numBars)
    {
@@ -40,7 +63,12 @@ std::optional<double> GetBpm(const std::vector<double>& odfVals, double playDur)
             xcorr += v * odfVals[k++];
       }
       const auto bpm = 4 * 60 * numBars / playDur;
-      Score score { 10 * std::log(xcorr / numBeats), GetBpmLogLikelihood(bpm) };
+      // This weight was chosen just by looking at xcorr and bpm scores while
+      // debugging. We might get something yielding better results through some
+      // regression technique.
+      constexpr auto xcorrWeight = 10;
+      Score score { xcorrWeight * std::log(xcorr / numBeats),
+                    GetBpmLogLikelihood(bpm) };
       scores.push_back(score);
    }
 
