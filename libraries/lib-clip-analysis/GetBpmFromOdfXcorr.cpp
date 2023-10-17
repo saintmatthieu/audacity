337d53a1c9d5460d222d9889f579515adc4085ec
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2023-10-17 21:47:40 +0200

    Stop recursion when divs match time signature

--- libraries/lib-clip-analysis/GetBpmFromOdfXcorr.cpp
+++ libraries/lib-clip-analysis/GetBpmFromOdfXcorr.cpp
@@ -87,27 +87,65 @@ GetEvalIndices(int M, int numDivs, const std::vector<int>& prevIndices)
    return evalIndices;
 }
 
-std::vector<float> GetValues(
-   const std::vector<float>& xcorr, const std::vector<int>& indices)
+std::vector<float>
+GetValues(const std::vector<float>& xcorr, const std::vector<int>& indices)
 {
    std::vector<float> values(indices.size());
-   std::transform(
-      indices.begin(), indices.end(), values.begin(),
-      [&](int i) { return xcorr[i]; });
+   std::transform(indices.begin(), indices.end(), values.begin(), [&](int i) {
+      return xcorr[i];
+   });
    return values;
+}
+
+enum class TimeSig
+{
+   FourFour,
+   ThreeFour,
+   SixEight,
+   _Count
+};
+
+static const std::array<std::vector<int>, static_cast<int>(TimeSig::_Count)>
+   timeSigDivs { std::vector<int> { 2, 2 }, std::vector<int> { 3 },
+                 std::vector<int> { 2, 3 } };
+
+bool IsTimeSigLevel(TimeSig sig, const std::vector<int>& divs)
+{
+   if (divs.empty())
+      return false;
+   const auto& sigDivs = timeSigDivs[static_cast<int>(sig)];
+   // We ignore the first entry in divs because it corresponds to the number of
+   // bars in the loop.
+   return sigDivs == std::vector<int> { divs.begin() + 1, divs.end() };
+}
+
+bool MatchesSomeTimeSig(const std::vector<int>& divs)
+{
+   constexpr std::array<TimeSig, static_cast<int>(TimeSig::_Count)> timeSigs {
+      TimeSig::FourFour, TimeSig::ThreeFour, TimeSig::SixEight
+   };
+   return std::any_of(timeSigs.begin(), timeSigs.end(), [&](TimeSig sig) {
+      return IsTimeSigLevel(sig, divs);
+   });
 }
 
 void recursion(
    const XCorr& xcorr, std::array<TimeDiv*, numPrimes>& subTimeDivs,
-   double cumScore = 1, int cumDiv = 1, int level = 1)
+   double cumScore = 1, std::vector<int> divs = {})
 {
    const auto M = xcorr.values.size();
+   const auto cumDiv =
+      std::accumulate(divs.begin(), divs.end(), 1, std::multiplies<int>());
    const auto prevIndices = DivideEqually(M, cumDiv);
    for (auto p = 0; p < numPrimes; ++p)
    {
       const auto newDiv = cumDiv * primes[p];
       if (newDiv > xcorr.numPeaks)
          continue;
+
+      auto nextDivs = divs;
+      nextDivs.push_back(primes[p]);
+
       const auto evalIndices = GetEvalIndices(M, newDiv, prevIndices);
       const auto values = GetValues(xcorr.values, evalIndices);
       const auto maxScore = *std::max_element(values.begin(), values.end());
@@ -116,7 +154,11 @@ void recursion(
       const auto varScore = 1. - GetStandardDeviation(values);
       const auto score = varScore * maxScore;
       subTimeDivs[p] = new TimeDiv(score, score * cumScore, maxScore, varScore);
-      recursion(xcorr, subTimeDivs[p]->subs, score * cumScore, newDiv, level + 1);
+      if (MatchesSomeTimeSig(nextDivs))
+         // We have the score for a time signature, we don't need to test its
+         // subdivisions.
+         continue;
+      recursion(xcorr, subTimeDivs[p]->subs, score * cumScore, nextDivs);
    }
 }
 
