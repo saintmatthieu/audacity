6e19a7b6cbfad0e68b5fbd725b9eb4bb4445ee23
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2023-12-02 20:06:10 +0100

    some GMM repo tentative check-in

---
+++ lib-src/Gaussian-Mixture-Model/cmake/MakeLibrary.cmake
@@ -0,0 +1,24 @@
+function(MakeLibrary LIBRARY_NAME INCLUDE_DIR)
+file(GLOB_RECURSE SOURCES 
+${CMAKE_CURRENT_SOURCE_DIR}/*.h 
+${CMAKE_CURRENT_SOURCE_DIR}/*.cpp
+)
+
+if(LIB_OPT)
+	if (WIN32)
+		set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS TRUE)
+	endif ()	
+	
+	add_library(${LIBRARY_NAME} SHARED ${SOURCES})
+else()
+	add_library(${LIBRARY_NAME} STATIC ${SOURCES})
+endif()
+
+target_include_directories(${LIBRARY_NAME} PUBLIC 
+	${CMAKE_CURRENT_SOURCE_DIR}/${INCLUDE_DIR}
+)
+
+install(TARGETS ${LIBRARY_NAME})
+install (DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${INCLUDE_DIR}/ DESTINATION include/${LIBRARY_NAME} FILES_MATCHING PATTERN "*.h*")
+endfunction()
+
