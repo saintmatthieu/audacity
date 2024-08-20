98dda0b387e6e56c40facb95b8f7d1f92cd0710b
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2024-08-20 10:52:35 +0200

    New "let-ring" state in AudioIO ...
    Set when calling `ProjectAudioManager::Stop(false /*stopStream*/)`.
    The audio thread still runs, but the playback schedule isn't updated.

--- src/ProjectAudioManager.cpp
+++ src/ProjectAudioManager.cpp
@@ -529,6 +529,8 @@ void ProjectAudioManager::Stop(bool stopStream /* = true*/)
 
    if(stopStream)
       gAudioIO->StopStream();
+   else
+      gAudioIO->SetLetRing(true);
 
    projectAudioManager.SetLooping( false );
    projectAudioManager.SetCutting( false );
